e703474655b480913b0e9130dc092b3cf10eb2a4
27581 <2758178527@qq.com>
2024-10-10 15:49:49 +0800

    init

---
+++ mall-api/src/main/java/edu/npu/mall/Main.java
@@ -0,0 +1,7 @@
+package edu.npu.mall;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ mall-user/src/main/java/edu/npu/mall/UserApplication.java
@@ -0,0 +1,13 @@
+package edu.npu.mall;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
